ee254dc3bf6b6750dba8d3f3c7f20997599a8578
arraylistnumstring <40125573+arraylistnumstring@users.noreply.github.com>
2024-03-25 10:09:37 -0400

    Fixed compilation errors

--- static-priority-search-tree.h
+++ static-priority-search-tree.h
@@ -4,31 +4,33 @@
 #include <concepts>		// To create abstract class template; requires C++20
 #include <iostream>
 
+// As this concept is not a type, using it as a constraint must be done with a trailing requires keyword
 template <typename T, template<typename, size_t, typename> class PointStructTemplate,
 			template<typename, template<typename, size_t, typename> class, size_t, typename> class StaticPrioritySearchTree,
 			size_t num_ID_fields=0, typename IDType=void
 		 >
-concept StaticPrioritySearchTree = requires (T t, PointStructTemplate<T, num_ID_fields, IDType> ptstr,
-												StaticPrioritySearchTree<T, PointStructTemplate, num_ID_fields, IDType> static_pst,
-												IDType id_type)
+concept StaticPST = requires (T t, PointStructTemplate<T, num_ID_fields, IDType> ptstr,
+								StaticPrioritySearchTree<T, PointStructTemplate, num_ID_fields, IDType> static_pst,
+								IDType id_type)
 {
 	// Printing function for printing operator << to use, as private data members must be accessed in the process
-	{static_pst.print(std::declval<std::ostream &os>())} -> void;
+	// {} -> type_func<> passes in result of curly braces to the first template parameter of type_func<>
+	{static_pst.print(std::declval<std::ostream &>())} -> std::is_void;
 
 	// Required search functions
 	{static_pst.threeSidedSearch(std::declval<size_t &>(/* num_res_elems */),
 									std::declval<T>(/* min_dim1_val */),
 									std::declval<T>(/* max_dim1_val */),
 									std::declval<T>(/* min_dim2_val */))}
-								-> PointStructTemplate<T, num_ID_fields, IDType>*;
+								-> std::same_as<PointStructTemplate<T, num_ID_fields, IDType>*>;
 	{static_pst.twoSidedLeftSearch(std::declval<size_t &>(/* num_res_elems */),
 									std::declval<T>(/* max_dim1_val */),
 									std::declval<T>(/* min_dim2_val */))}
-								-> PointStructTemplate<T, num_ID_fields, IDType>*;
+								-> std::same_as<PointStructTemplate<T, num_ID_fields, IDType>*>;
 	{static_pst.twoSidedRightSearch(std::declval<size_t &>(/* num_res_elems */),
 									std::declval<T>(/* min_dim1_val */),
 									std::declval<T>(/* min_dim2_val */))}
-								-> PointStructTemplate<T, num_ID_fields, IDType>*;
+								-> std::same_as<PointStructTemplate<T, num_ID_fields, IDType>*>;
 };
 
 template <class T>
