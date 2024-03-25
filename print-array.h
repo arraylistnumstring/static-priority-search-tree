d136a2b418629a6589b66145fddc194da7e9c7dc
arraylistnumstring <40125573+arraylistnumstring@users.noreply.github.com>
2024-03-25 02:14:48 -0400

    Edited typo in print-array.h erroneously taking in and returning an ostream instead of an ostream reference

--- print-array.h
+++ print-array.h
@@ -4,19 +4,19 @@
 #include <iostream>
 
 template <typename T>
-std::ostream printArray(std::ostream os, T *const &T_arr, const size_t start_ind, const size_t num_elems);
+std::ostream &printArray(std::ostream &os, T *const &T_arr, const size_t start_ind, const size_t num_elems);
 
 template <typename T>
-std::ostream printArrayDereferenced(std::ostream os, T **const &T_ptr_arr, const size_t start_ind, const size_t num_elems);
+std::ostream &printArrayDereferenced(std::ostream &os, T **const &T_ptr_arr, const size_t start_ind, const size_t num_elems);
 
 template <typename T>
-std::ostream printArrayIndexed(std::ostream os, T *const &T_arr, size_t *const &ind_arr, const size_t start_ind, const size_t num_elems);
+std::ostream &printArrayIndexed(std::ostream &os, T *const &T_arr, size_t *const &ind_arr, const size_t start_ind, const size_t num_elems);
 
 template <typename T>
-std::ostream printArrayOffsetFromStart(std::ostream os, T *const &T_arr, T **const &T_ptr_arr, const size_t start_ind, const size_t num_elems);
+std::ostream &printArrayOffsetFromStart(std::ostream &os, T *const &T_arr, T **const &T_ptr_arr, const size_t start_ind, const size_t num_elems);
 
 template <typename T>
-std::ostream printArray(std::ostream os, T *const &T_arr, const size_t start_ind, const size_t num_elems)
+std::ostream &printArray(std::ostream &os, T *const &T_arr, const size_t start_ind, const size_t num_elems)
 {
 	os << "[ ";
 	for (size_t i = start_ind; i < start_ind + num_elems; i++)
@@ -27,7 +27,7 @@ std::ostream printArray(std::ostream os, T *const &T_arr, const size_t start_ind
 
 // For printing the content referenced by an array of pointers
 template <typename T>
-std::ostream printArrayDereferenced(std::ostream os, T **const &T_ptr_arr, const size_t start_ind, const size_t num_elems)
+std::ostream &printArrayDereferenced(std::ostream &os, T **const &T_ptr_arr, const size_t start_ind, const size_t num_elems)
 {
 	os << "[ ";
 	for (size_t i = start_ind; i < start_ind + num_elems; i++)
@@ -39,7 +39,7 @@ std::ostream printArrayDereferenced(std::ostream os, T **const &T_ptr_arr, const
 
 // For printing one array using another's elements as indices
 template <typename T>
-std::ostream printArrayIndexed(std::ostream os, T *const &T_arr, size_t *const &ind_arr, const size_t start_ind, size_t num_elems)
+std::ostream &printArrayIndexed(std::ostream &os, T *const &T_arr, size_t *const &ind_arr, const size_t start_ind, size_t num_elems)
 {
 	os << "[ ";
 	for (size_t i = start_ind; i < start_ind + num_elems; i++)
@@ -50,7 +50,7 @@ std::ostream printArrayIndexed(std::ostream os, T *const &T_arr, size_t *const &
 
 // For printing the indices of one array according to the ordering provided by an array of pointers to it
 template <typename T>
-std::ostream printArrayOffsetFromStart(std::ostream os, T *const &T_arr, T **const &T_ptr_arr, const size_t start_ind, const size_t num_elems)
+std::ostream &printArrayOffsetFromStart(std::ostream &os, T *const &T_arr, T **const &T_ptr_arr, const size_t start_ind, const size_t num_elems)
 {
 	os << "[ ";
 	for (size_t i = start_ind; i < start_ind + num_elems; i++)
