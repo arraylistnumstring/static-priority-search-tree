cb9aea7c2706768b47cc5cc4e3b80b450452e4f4
arraylistnumstring <40125573+arraylistnumstring@users.noreply.github.com>
2024-04-15 18:18:05 -0400

    Added modified code from original marchingCubes.cpp, after removing references to GLUT and interactive-only features

--- marching-cubes/marchingCubes.cpp
+++ marching-cubes/marchingCubes.cpp
@@ -323,5 +323,280 @@ void runGraphicsTest(int argc, char **argv) {
 
 	display();
 
+	// saveToFile();
+
 	cleanup();
+}
+
+#define DEBUG_BUFFERS 0
+
+////////////////////////////////////////////////////////////////////////////////
+//! Run the Cuda part of the computation
+////////////////////////////////////////////////////////////////////////////////
+void computeIsosurface() {
+	int threads = 128;
+	dim3 grid(numVoxels / threads, 1, 1);
+
+	// get around maximum grid size of 65535 in each dimension
+	if (grid.x > 65535) {
+		grid.y = grid.x / 32768;
+		grid.x = 32768;
+	}
+
+	// calculate number of vertices need per voxel
+	launch_classifyVoxel(grid, threads, d_voxelVerts, d_voxelOccupied, d_volume,
+			gridSize, gridSizeShift, gridSizeMask, numVoxels,
+			voxelSize, isoValue);
+#if DEBUG_BUFFERS
+	printf("voxelVerts:\n");
+	dumpBuffer(d_voxelVerts, numVoxels, sizeof(uint));
+#endif
+
+#if SKIP_EMPTY_VOXELS
+	// scan voxel occupied array
+	ThrustScanWrapper(d_voxelOccupiedScan, d_voxelOccupied, numVoxels);
+
+#if DEBUG_BUFFERS
+	printf("voxelOccupiedScan:\n");
+	dumpBuffer(d_voxelOccupiedScan, numVoxels, sizeof(uint));
+#endif
+
+	// read back values to calculate total number of non-empty voxels
+	// since we are using an exclusive scan, the total is the last value of
+	// the scan result plus the last value in the input array
+	{
+		uint lastElement, lastScanElement;
+		checkCudaErrors(cudaMemcpy((void *)&lastElement,
+					(void *)(d_voxelOccupied + numVoxels - 1),
+					sizeof(uint), cudaMemcpyDeviceToHost));
+		checkCudaErrors(cudaMemcpy((void *)&lastScanElement,
+					(void *)(d_voxelOccupiedScan + numVoxels - 1),
+					sizeof(uint), cudaMemcpyDeviceToHost));
+		activeVoxels = lastElement + lastScanElement;
+	}
+
+	if (activeVoxels == 0) {
+		// return if there are no full voxels
+		totalVerts = 0;
+		return;
+	}
+
+	// compact voxel index array
+	launch_compactVoxels(grid, threads, d_compVoxelArray, d_voxelOccupied,
+			d_voxelOccupiedScan, numVoxels);
+	getLastCudaError("compactVoxels failed");
+
+#endif  // SKIP_EMPTY_VOXELS
+
+	// scan voxel vertex count array
+	ThrustScanWrapper(d_voxelVertsScan, d_voxelVerts, numVoxels);
+
+#if DEBUG_BUFFERS
+	printf("voxelVertsScan:\n");
+	dumpBuffer(d_voxelVertsScan, numVoxels, sizeof(uint));
+#endif
+
+	// readback total number of vertices
+	{
+		uint lastElement, lastScanElement;
+		checkCudaErrors(cudaMemcpy((void *)&lastElement,
+					(void *)(d_voxelVerts + numVoxels - 1),
+					sizeof(uint), cudaMemcpyDeviceToHost));
+		checkCudaErrors(cudaMemcpy((void *)&lastScanElement,
+					(void *)(d_voxelVertsScan + numVoxels - 1),
+					sizeof(uint), cudaMemcpyDeviceToHost));
+		totalVerts = lastElement + lastScanElement;
+	}
+
+	// generate triangles, writing to vertex buffers
+	size_t num_bytes;
+	checkCudaErrors(cudaGraphicsMapResources(1, &cuda_posvbo_resource, 0));
+	checkCudaErrors(cudaGraphicsResourceGetMappedPointer(
+				(void **)&d_pos, &num_bytes, cuda_posvbo_resource));
+	checkCudaErrors(cudaGraphicsMapResources(1, &cuda_normalvbo_resource, 0));
+	checkCudaErrors(cudaGraphicsResourceGetMappedPointer(
+				(void **)&d_normal, &num_bytes, cuda_normalvbo_resource));
+
+#if SKIP_EMPTY_VOXELS
+	dim3 grid2((int)ceil(activeVoxels / (float)NTHREADS), 1, 1);
+#else
+	dim3 grid2((int)ceil(numVoxels / (float)NTHREADS), 1, 1);
+#endif
+
+	while (grid2.x > 65535) {
+		grid2.x /= 2;
+		grid2.y *= 2;
+	}
+
+#if SAMPLE_VOLUME
+	launch_generateTriangles2(grid2, NTHREADS, d_pos, d_normal, d_compVoxelArray,
+			d_voxelVertsScan, d_volume, gridSize, gridSizeShift,
+			gridSizeMask, voxelSize, isoValue, activeVoxels,
+			maxVerts);
+#else
+	launch_generateTriangles(grid2, NTHREADS, d_pos, d_normal, d_compVoxelArray,
+			d_voxelVertsScan, gridSize, gridSizeShift,
+			gridSizeMask, voxelSize, isoValue, activeVoxels,
+			maxVerts);
+#endif
+
+	checkCudaErrors(cudaGraphicsUnmapResources(1, &cuda_normalvbo_resource, 0));
+	checkCudaErrors(cudaGraphicsUnmapResources(1, &cuda_posvbo_resource, 0));
+}
+
+// shader for displaying floating-point texture
+static const char *shader_code =
+"!!ARBfp1.0\n"
+"TEX result.color, fragment.texcoord, texture[0], 2D; \n"
+"END";
+
+GLuint compileASMShader(GLenum program_type, const char *code) {
+	GLuint program_id;
+	glGenProgramsARB(1, &program_id);
+	glBindProgramARB(program_type, program_id);
+	glProgramStringARB(program_type, GL_PROGRAM_FORMAT_ASCII_ARB,
+			(GLsizei)strlen(code), (GLubyte *)code);
+
+	GLint error_pos;
+	glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &error_pos);
+
+	if (error_pos != -1) {
+		const GLubyte *error_string;
+		error_string = glGetString(GL_PROGRAM_ERROR_STRING_ARB);
+		fprintf(stderr, "Program error at position: %d\n%s\n", (int)error_pos,
+				error_string);
+		return 0;
+	}
+
+	return program_id;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Initialize OpenGL
+////////////////////////////////////////////////////////////////////////////////
+bool initGL(int *argc, char **argv) {
+	// Create GL context; may need to replace this portion with EGL or some headless OpenGL context creator
+	if (!isGLVersionSupported(2, 0)) {
+		fprintf(stderr, "ERROR: Support for necessary OpenGL extensions missing.");
+		fflush(stderr);
+		return false;
+	}
+
+	// default initialization
+	glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+
+	// good old-fashioned fixed function lighting
+	float black[] = {0.0f, 0.0f, 0.0f, 1.0f};
+	float white[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	float ambient[] = {0.1f, 0.1f, 0.1f, 1.0f};
+	float diffuse[] = {0.9f, 0.9f, 0.9f, 1.0f};
+	float lightPos[] = {0.0f, 0.0f, 1.0f, 0.0f};
+
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, black);
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, white);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, white);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, black);
+
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+
+	// load shader program
+	gl_Shader = compileASMShader(GL_FRAGMENT_PROGRAM_ARB, shader_code);
+
+	return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Create VBO
+////////////////////////////////////////////////////////////////////////////////
+void createVBO(GLuint *vbo, unsigned int size) {
+	// create buffer object
+	glGenBuffers(1, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, *vbo);
+
+	// initialize buffer object
+	glBufferData(GL_ARRAY_BUFFER, size, 0, GL_DYNAMIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glutReportErrors();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Delete VBO
+////////////////////////////////////////////////////////////////////////////////
+void deleteVBO(GLuint *vbo, struct cudaGraphicsResource **cuda_resource) {
+	glBindBuffer(1, *vbo);
+	glDeleteBuffers(1, vbo);
+	// DEPRECATED: checkCudaErrors(cudaGLUnregisterBufferObject(*vbo));
+	cudaGraphicsUnregisterResource(*cuda_resource);
+
+	*vbo = 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Render isosurface geometry from the vertex buffers
+////////////////////////////////////////////////////////////////////////////////
+void renderIsosurface() {
+	glBindBuffer(GL_ARRAY_BUFFER, posVbo);
+	glVertexPointer(4, GL_FLOAT, 0, 0);
+	glEnableClientState(GL_VERTEX_ARRAY);
+
+	glBindBuffer(GL_ARRAY_BUFFER, normalVbo);
+	glNormalPointer(GL_FLOAT, sizeof(float) * 4, 0);
+	glEnableClientState(GL_NORMAL_ARRAY);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glDrawArrays(GL_TRIANGLES, 0, totalVerts);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_NORMAL_ARRAY);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Display callback
+////////////////////////////////////////////////////////////////////////////////
+void display() {
+	sdkStartTimer(&timer);
+
+	// run CUDA kernel to generate geometry
+	if (compute) {
+		computeIsosurface();
+	}
+
+	// Common display code path
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// set view matrix
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glTranslatef(translate.x, translate.y, translate.z);
+		glRotatef(rotate.x, 1.0, 0.0, 0.0);
+		glRotatef(rotate.y, 0.0, 1.0, 0.0);
+
+		glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
+
+		if (lighting) {
+			glEnable(GL_LIGHTING);
+		}
+
+		// render
+		if (render) {
+			glPushMatrix();
+			glRotatef(180.0, 0.0, 1.0, 0.0);
+			glRotatef(90.0, 1.0, 0.0, 0.0);
+			renderIsosurface();
+			glPopMatrix();
+		}
+
+		glDisable(GL_LIGHTING);
+	}
 }
