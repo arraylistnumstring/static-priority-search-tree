a84ec464987bf7afe1adaec95b8927c3f19cdd3b
arraylistnumstring <40125573+arraylistnumstring@users.noreply.github.com>
2024-11-30 05:07:21 -0500

    Completed binary input file reader (compilation not yet tested)

--- helpers/binary-input-file-reader.h
+++ helpers/binary-input-file-reader.h
@@ -3,6 +3,7 @@
 
 #include <fstream>
 
+#include "exit-status-codes.h"
 #include "preprocessor-symbols.h"
 
 template <typename T, typename GridDimType>
@@ -14,9 +15,24 @@ T *readInVertices(std::string input_filename, GridDimType grid_dims[NUM_DIMS])
 
 	T *vertex_arr = new T[num_vertices];
 
-	std::ifstream input_filestream {input_filename, std::ios_base::binary};
+	std::ifstream input_filestream(input_filename, std::ios_base::binary);
 
-	input_filestream.read()
+	if (!input_filestream.is_open())	// Check that filestream is open
+	{
+		std::cerr << "Failed to open " << input_filename << " for reading\n";
+		std::exit(ExitStatusCodes::FILE_OPEN_ERR);
+	}
+
+	input_filestream.read(reinterpret_cast<char *>(vertex_arr), num_vertices * sizeof(T));
+
+	// Check that the appropriate number of bytes were read
+	if (input_filestream.gcount() != num_vertices * sizeof(T))
+	{
+		std::cerr << "Failure: read " << input_filestream.gcount() << " bytes, expected " << num_vertices * sizeof(T) << " bytes\n";
+		std::exit(ExitStatusCodes::INPUT_READ_ERR);
+	}
+
+	return vertex_arr;
 };
 
 #endif
