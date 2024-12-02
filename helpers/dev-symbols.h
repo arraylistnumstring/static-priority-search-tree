60bb5e5cfe11a34287c0f6fbc857814d6f9722e2
arraylistnumstring <40125573+arraylistnumstring@users.noreply.github.com>
2024-12-02 17:48:12 -0500

    Updated technical specification information based on compute capabilities

--- helpers/dev-symbols.h
+++ helpers/dev-symbols.h
@@ -8,6 +8,10 @@ __device__ unsigned long long res_arr_ind_d;
 // To track number of active resident grids for purpose of determining whether to call dynamic parallel functions with cudaStreamFireAndForget or to simply use the default device stream (as an excessive number of the former causes resource allocation failures and therefore correctness issues)
 __device__ unsigned int num_active_grids_d;
 
+// Source for compute capability-specific technical specifications:
+//	Table 21 of
+//		https://docs.nvidia.com/cuda/cuda-c-programming-guide/#features-and-technical-specifications
+
 // __CUDA_ARCH__ is a preprocessor variable defined only on device that evaluates to XY0 for a device of compute capability X.Y
 // To save memory (especially duplication of value across host and device), simply declare MAX_NUM_ACTIVE_GRIDS a preprocessor variable
 #if __CUDA_ARCH__ == 600 || __CUDA_ARCH__ == 700 || __CUDA_ARCH__ >= 750
@@ -18,5 +22,15 @@ __device__ unsigned int num_active_grids_d;
 #else // __CUDA_ARCH__ == 530 || __CUDA_ARCH__ == 620 || __CUDA_ARCH__ = 720
 	#define MAX_NUM_ACTIVE_GRIDS 16
 #endif
+
+#define MAX_X_DIM_NUM_BLOCKS ((1 << 31) - 1)
+#define MAX_Y_DIM_NUM_BLOCKS ((1 << 16) - 1)
+#define MAX_Z_DIM_NUM_BLOCKS ((1 << 16) - 1)
+
+#define MAX_X_DIM_THREADS_PER_BLOCK 1024
+#define MAX_Y_DIM_THREADS_PER_BLOCK 1024
+#define MAX_Z_DIM_THREADS_PER_BLOCK 64
+
+#define MAX_THREADS_PER_BLOCK 1024
 
 #endif
