41e1dac4b4e8e97d33839ccb5b1b0ecfdc2e454e
arraylistnumstring <40125573+arraylistnumstring@users.noreply.github.com>
2025-01-12 22:06:26 -0500

    Added help menu note regarding correctness of program with respect to metacell size

--- testers/pst-dataset-tester-driver.cu
+++ testers/pst-dataset-tester-driver.cu
@@ -63,6 +63,7 @@ int main(int argc, char *argv[])
 
 			std::cerr << "\t-m, --metacell-dims METACELL_DIM_X [METACELL_DIM_Y METACELL_DIM_Z]\n";
 			std::cerr << "\t\t\tDimensions to use for metacells, in units of voxels^3; if only METACELL_DIM_X is provided, METACELL_DIM_Y and METACELL_DIM_Z are set to be equal to the same value; metacell dimensions default to 4 * 4 * 4 voxels^3 (as this number is both a cubic number and a multiple of 32 (warp size, for maximal thread occupancy on the GPU), and is the smallest possible metacell satisfying both those criteria, as a smaller metacell yields more metacells, a regime where PST performs well)\n\n";
+			std::cerr << "\t\t\t\tCurrently only guaranteed to work for metacells of dimensions 4 * 4 * 4 voxels^3\n\n\n\n";
 
 			std::cerr << "\t-r, --report-IDs\tWhether to report point IDs or full info of a point; defaults to full info\n\n";
 
