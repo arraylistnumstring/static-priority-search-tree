b53fcfc79fb83871d84eb2841f322cc2dc074811
arraylistnumstring <40125573+arraylistnumstring@users.noreply.github.com>
2025-01-15 23:20:10 -0500

    Extracted StaticPSTCPURecur<...>::TreeNode into template class CPURecurTreeNode for parity with new TreeNode design for PSTs (and due to lack of need for nesting)

---
+++ pst/cpu-recur-tree-node.h
@@ -0,0 +1,86 @@
+#ifndef CPU_RECUR_TREE_NODE
+#define CPU_RECUR_TREE_NODE
+
+#include "class-member-checkers.h"
+
+
+template <typename T, template<typename, typename, size_t> class PointStructTemplate,
+			typename IDType, size_t num_IDs
+		>
+class CPURecurTreeNode
+{
+	public:
+		CPURecurTreeNode()
+			// Value initialisation is more efficient with member initialiser lists, as they are not default-initialised before being overriden
+			// When no members are explicitly initialised, default-initialisation of non-class variables with automatic or dynamic storage duration produces objects with indeterminate values
+			: median_dim1_val(0),
+			code(0)
+		{};
+
+		CPURecurTreeNode(PointStructTemplate<T, IDType, num_IDs> &source_data, T median_dim1_val)
+			// When other subobjects are explicitly initialised, those that are not are implicitly initialised in the same way as objects with static storage duration, i.e. with 0 or nullptr (stated in 6.7.8 (19) of the C++ standard)
+			: pt(source_data),
+			median_dim1_val(median_dim1_val)
+		{};
+
+		virtual ~CPURecurTreeNode() {};
+
+		CPURecurTreeNode& operator=(CPURecurTreeNode &source)	// assignment operator
+		{
+			if (this == &source)
+				return *this;	// If the two addresses match, it's the same object
+
+			pt = source.pt;
+			median_dim1_val = source.median_dim1_val;
+			code = source.code;
+
+			return *this;
+		};
+
+		CPURecurTreeNode(CPURecurTreeNode &node);	// copy constructor
+
+		// Printing function for printing operator << to use, as private data members must be accessed in the process
+		// const keyword after method name indicates that the method does not modify any data members of the associated class
+		virtual void print(std::ostream &os) const
+		{
+			os << '(' << pt.dim1_val << ", " << pt.dim2_val  << "; " << median_dim1_val;
+			if constexpr (HasID<PointStructTemplate<T, IDType, num_IDs>>::value)
+				os << "; " << pt.id;
+			os << ')';
+		};
+
+		void setTreeNode(PointStructTemplate<T, IDType, num_IDs> &source_data, T median_dim1_val)
+		{
+			pt = source_data;	// assignment operator invoked
+			this->median_dim1_val = median_dim1_val;
+		};
+
+		// Current index is this-root
+		// Addition and subtraction (+, -) have higher precedence than bitshift operators (<<, >>)
+		// root needs to be passed into get*() functions as nested class are not connected to any instances of the outer class in C++
+		inline CPURecurTreeNode &getLeftChild(CPURecurTreeNode *root) const {return root[(this-root << 1) + 1];};
+		inline CPURecurTreeNode &getRightChild(CPURecurTreeNode *root) const {return root[(this-root << 1) + 2];};
+		inline CPURecurTreeNode &getParent(CPURecurTreeNode *root) const {return root[this-root-1 >> 1];};
+		inline bool hasChildren() const {return static_cast<bool> (code & (HAS_LEFT_CHILD | HAS_RIGHT_CHILD));};
+		inline bool hasLeftChild() const {return static_cast<bool> (code & HAS_LEFT_CHILD);};
+		inline bool hasRightChild() const {return static_cast<bool> (code & HAS_RIGHT_CHILD);};
+		inline void setLeftChild() {code |= HAS_LEFT_CHILD;};
+		inline void setRightChild() {code |= HAS_RIGHT_CHILD;};
+		inline void unsetLeftChild() {code &= ~HAS_LEFT_CHILD;};
+		inline void unsetRightChild() {code &= ~HAS_RIGHT_CHILD;};
+
+		PointStructTemplate<T, IDType, num_IDs> pt;
+		T median_dim1_val;
+		// bool (1 byte) auto-converts any values other than 0 or 1 to 1, so another type is necessary; as the struct takes the alignment requirement of the largest enclosed data type, and typical use cases mean that T will be at least as big as an int or a float (4B), it is fine to use an unsigned int (4B) to store the bitcode
+		unsigned code;
+
+		// Bitcodes used to indicate presence of left/right children (and potentially other values as necessary) to save space, as bool actually takes up 1 byte, same as a char
+		// Without an explicit instantiation, Bitcodes won't take up any space
+		enum Bitcodes
+		{
+			HAS_LEFT_CHILD = 0x2,
+			HAS_RIGHT_CHILD = 0x1
+		};
+};
+
+#endif
--- pst/static-pst-cpu-recur.h
+++ pst/static-pst-cpu-recur.h
@@ -1,6 +1,7 @@
 #ifndef STATIC_PST_CPU_RECUR_H
 #define STATIC_PST_CPU_RECUR_H
 
+#include "cpu-recur-tree-node.h"
 #include "resize-array.h"
 #include "static-priority-search-tree.h"
 
@@ -95,18 +96,18 @@ class StaticPSTCPURecur : public StaticPrioritySearchTree<T, PointStructTemplate
 		StaticPSTCPURecur(StaticPSTCPURecur &tree);	// copy constructor
 
 		// Declare nested class and define in implementation file
-		class TreeNode;
+		class CPURecurTreeNode;
 
-		TreeNode *root;
+		CPURecurTreeNode *root;
 
 		// Recursive constructor helper to populate the tree
 		// From the specification of C, pointers are const if the const qualifier appears to the right of the corresponding *
 		// Note that **const ensures that a second-level pointer (**) is constant, but that the pointer to which it points (i.e. a first-level pointer, *) is not
-		void populateTreeRecur(TreeNode &subtree_root, PointStructTemplate<T, IDType, num_IDs> *const *const dim1_val_ptr_subarr, PointStructTemplate<T, IDType, num_IDs> *const *const dim2_val_ptr_subarr, const size_t num_elems);
+		void populateTreeRecur(CPURecurTreeNode &subtree_root, PointStructTemplate<T, IDType, num_IDs> *const *const dim1_val_ptr_subarr, PointStructTemplate<T, IDType, num_IDs> *const *const dim2_val_ptr_subarr, const size_t num_elems);
 		// Returns index in dim1_val_ptr_arr of elem_to_find
 		long long binarySearch(PointStructTemplate<T, IDType, num_IDs> *const *const dim1_val_ptr_arr, PointStructTemplate<T, IDType, num_IDs> &elem_to_find, const size_t num_elems);
 
-		void printRecur(std::ostream &os, const TreeNode &subtree_root, std::string prefix, std::string child_prefix) const;
+		void printRecur(std::ostream &os, const CPURecurTreeNode &subtree_root, std::string prefix, std::string child_prefix) const;
 
 		// Search-related helper functions
 		template <typename RetType>
@@ -114,30 +115,30 @@ class StaticPSTCPURecur : public StaticPrioritySearchTree<T, PointStructTemplate
 								std::is_same<RetType, IDType>,
 								std::is_same<RetType, PointStructTemplate<T, IDType, num_IDs>>
 				>::value
-		void threeSidedSearchRecur(RetType *&res_arr, size_t &num_res_elems, size_t &res_arr_size, TreeNode &subtree_root, T min_dim1_val, T max_dim1_val, T min_dim2_val);
+		void threeSidedSearchRecur(RetType *&res_arr, size_t &num_res_elems, size_t &res_arr_size, CPURecurTreeNode &subtree_root, T min_dim1_val, T max_dim1_val, T min_dim2_val);
 		template <typename RetType>
 			requires std::disjunction<
 								std::is_same<RetType, IDType>,
 								std::is_same<RetType, PointStructTemplate<T, IDType, num_IDs>>
 				>::value
-		void twoSidedLeftSearchRecur(RetType *&res_arr, size_t &num_res_elems, size_t &res_arr_size, TreeNode &subtree_root, T max_dim1_val, T min_dim2_val);
+		void twoSidedLeftSearchRecur(RetType *&res_arr, size_t &num_res_elems, size_t &res_arr_size, CPURecurTreeNode &subtree_root, T max_dim1_val, T min_dim2_val);
 		template <typename RetType>
 			requires std::disjunction<
 								std::is_same<RetType, IDType>,
 								std::is_same<RetType, PointStructTemplate<T, IDType, num_IDs>>
 				>::value
-		void twoSidedRightSearchRecur(RetType *&res_arr, size_t &num_res_elems, size_t &res_arr_size, TreeNode &subtree_root, T min_dim1_val, T min_dim2_val);
+		void twoSidedRightSearchRecur(RetType *&res_arr, size_t &num_res_elems, size_t &res_arr_size, CPURecurTreeNode &subtree_root, T min_dim1_val, T min_dim2_val);
 		template <typename RetType>
 			requires std::disjunction<
 								std::is_same<RetType, IDType>,
 								std::is_same<RetType, PointStructTemplate<T, IDType, num_IDs>>
 				>::value
-		void reportAllNodes(RetType *&res_arr, size_t &num_res_elems, size_t &res_arr_size, TreeNode &subtree_root, T min_dim2_val);
+		void reportAllNodes(RetType *&res_arr, size_t &num_res_elems, size_t &res_arr_size, CPURecurTreeNode &subtree_root, T min_dim2_val);
 
-	// Allow printing operator << to be declared for TreeNode
+	// Allow printing operator << to be declared for CPURecurTreeNode
 	// For friend functions of template classes, for the compiler to recognise the function as a template function, it is necessary to either pre-declare each template friend function before the template class and modify the class-internal function declaration with an additional <> between the operator and the parameter list; or to simply define the friend function when it is declared
 	//	https://isocpp.org/wiki/faq/templates#template-friends
-	friend std::ostream &operator<<(std::ostream &os, const TreeNode &tn)
+	friend std::ostream &operator<<(std::ostream &os, const CPURecurTreeNode &tn)
 	{
 		tn.print(os);
 		return os;
@@ -145,7 +146,6 @@ class StaticPSTCPURecur : public StaticPrioritySearchTree<T, PointStructTemplate
 };
 
 // Implementation file; for class templates, implementations must be in the same file as the declaration so that the compiler can access them
-#include "static-pst-cpu-recur-tree-node.tpp"
 #include "static-pst-cpu-recur.tpp"
 
 #endif
