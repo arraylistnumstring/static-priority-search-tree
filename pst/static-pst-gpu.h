d91c874a71b54d660bab0477cd00de7411b1243e
arraylistnumstring <40125573+arraylistnumstring@users.noreply.github.com>
2025-02-07 13:16:26 -0500

    Updated indentation

--- pst/static-pst-gpu.h
+++ pst/static-pst-gpu.h
@@ -167,11 +167,11 @@ class StaticPSTGPU: public StaticPrioritySearchTree<T, PointStructTemplate, IDTy
 		// From the specification of C, pointers are const if the const qualifier appears to the right of the corresponding *
 		// Returns index in dim1_val_ind_arr of elem_to_find
 		__forceinline__ __device__ static long long binarySearch(PointStructTemplate<T, IDType, num_IDs> *const &pt_arr_d,
-																			size_t *const &dim1_val_ind_arr,
-																			PointStructTemplate<T, IDType, num_IDs> &elem_to_find,
-																			const size_t &init_ind,
-																			const size_t &num_elems
-																		);
+																	size_t *const &dim1_val_ind_arr,
+																	PointStructTemplate<T, IDType, num_IDs> &elem_to_find,
+																	const size_t &init_ind,
+																	const size_t &num_elems
+																);
 
 		__forceinline__ __device__ static void constructNode(T *const &root_d,
 																const size_t &num_elem_slots,
