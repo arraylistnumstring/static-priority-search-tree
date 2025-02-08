498481d7a9a9482fab254fdc2141da990cf6b4b1
arraylistnumstring <40125573+arraylistnumstring@users.noreply.github.com>
2025-02-07 21:02:35 -0500

    Added section header comments and rearranged function declaration ordering; added explanatory comment for calcTotArrSizeNumIDTypes()

--- pst/static-pst-cpu-iter.h
+++ pst/static-pst-cpu-iter.h
@@ -27,9 +27,13 @@ class StaticPSTCPUIter : public StaticPrioritySearchTree<T, PointStructTemplate,
 		// Since arrays were allocated continguously, only need to free one of the array pointers
 		virtual ~StaticPSTCPUIter() {if (num_elems != 0) delete[] root;};
 
+
 		// Printing function for printing operator << to use, as private data members must be accessed in the process
 		// const keyword after method name indicates that the method does not modify any data members of the associated class
 		virtual void print(std::ostream &os) const;
+
+
+		// Public search functions
 
 		// Uses stacks instead of recursion or dynamic parallelism
 		template <typename RetType=PointStructTemplate<T, IDType, num_IDs>>
@@ -39,12 +43,14 @@ class StaticPSTCPUIter : public StaticPrioritySearchTree<T, PointStructTemplate,
 				>::value
 		void threeSidedSearch(size_t &num_res_elems, RetType *&res_arr, T min_dim1_val,
 								T max_dim1_val, T min_dim2_val);
+
 		template <typename RetType=PointStructTemplate<T, IDType, num_IDs>>
 			requires std::disjunction<
 								std::is_same<RetType, IDType>,
 								std::is_same<RetType, PointStructTemplate<T, IDType, num_IDs>>
 				>::value
 		void twoSidedLeftSearch(size_t &num_res_elems, RetType *&res_arr, T max_dim1_val, T min_dim2_val);
+
 		template <typename RetType=PointStructTemplate<T, IDType, num_IDs>>
 			requires std::disjunction<
 								std::is_same<RetType, IDType>,
@@ -58,15 +64,7 @@ class StaticPSTCPUIter : public StaticPrioritySearchTree<T, PointStructTemplate,
 		StaticPSTCPUIter(StaticPSTCPUIter &tree);	// copy constructor
 
 
-		static void setNode(T *const root, const size_t node_ind, const size_t num_elem_slots,
-							PointStructTemplate<T, IDType, num_IDs> &source_data, T median_dim1_val)
-		{
-			getDim1ValsRoot(root, num_elem_slots)[node_ind] = source_data.dim1_val;
-			getDim2ValsRoot(root, num_elem_slots)[node_ind] = source_data.dim2_val;
-			getMedDim1ValsRoot(root, num_elem_slots)[node_ind] = median_dim1_val;
-			if constexpr (HasID<PointStructTemplate<T, IDType, num_IDs>>::value)
-				getIDsRoot(root, num_elem_slots)[node_ind] = source_data.id;
-		};
+		// Construction-related helper functions
 
 		static void constructNode(T *const &root,
 									const size_t &num_elem_slots,
@@ -83,6 +81,19 @@ class StaticPSTCPUIter : public StaticPrioritySearchTree<T, PointStructTemplate,
 									size_t &right_subarr_start_ind,
 									size_t &right_subarr_num_elems
 								);
+
+		static void setNode(T *const root, const size_t node_ind, const size_t num_elem_slots,
+							PointStructTemplate<T, IDType, num_IDs> &source_data, T median_dim1_val)
+		{
+			getDim1ValsRoot(root, num_elem_slots)[node_ind] = source_data.dim1_val;
+			getDim2ValsRoot(root, num_elem_slots)[node_ind] = source_data.dim2_val;
+			getMedDim1ValsRoot(root, num_elem_slots)[node_ind] = median_dim1_val;
+			if constexpr (HasID<PointStructTemplate<T, IDType, num_IDs>>::value)
+				getIDsRoot(root, num_elem_slots)[node_ind] = source_data.id;
+		};
+
+
+		// Search-related helper functions
 
 		// Helper functions for tracking work to be completed
 		void do3SidedSearchDelegation(const unsigned char &curr_node_bitcode, T min_dim1_val,
@@ -109,6 +120,9 @@ class StaticPSTCPUIter : public StaticPrioritySearchTree<T, PointStructTemplate,
 										std::stack<unsigned char> &search_codes_stack
 									);
 
+
+		// Data-accessing helper functions
+
 		// Helper functions for getting start indices for various arrays
 		static T* getDim1ValsRoot(T *const root, const size_t num_elem_slots)
 			{return root;};
@@ -129,11 +143,14 @@ class StaticPSTCPUIter : public StaticPrioritySearchTree<T, PointStructTemplate,
 					return reinterpret_cast<unsigned char*>(getIDsRoot(root, num_elem_slots) + num_ID_subarrs * num_elem_slots);
 			};	
 
+
+		// Data footprint calculation functions
+
 		// Helper function for calculating the number of elements of size T necessary to instantiate an array for root of tree
 		template <size_t num_T_subarrs>
 		static size_t calcTotArrSizeNumTs(const size_t num_elem_slots);
 
-		// Helper function for calculating the number of elements of size IDType necessary to instantiate an array for root of tree
+		// Helper function for calculating the number of elements of size IDType necessary to instantiate an array for root of tree; calculation differs from calcTotArrSizeNumTs() due to need for IDType alignment to be satisfied when sizeof(IDType) > sizeof(T)
 		template <size_t num_T_subarrs>
 			requires NonVoidType<IDType>
 		static size_t calcTotArrSizeNumIDTypes(const size_t num_elem_slots);
@@ -160,10 +177,13 @@ class StaticPSTCPUIter : public StaticPrioritySearchTree<T, PointStructTemplate,
 										const size_t &init_ind, const size_t &num_elems
 									);
 
+
 		void printRecur(std::ostream &os, T *const &tree_root, const size_t curr_ind,
 						const size_t num_elem_slots, std::string prefix, std::string child_prefix
 					) const;
 
+
+		// Data members
 
 		/*
 			Implicit tree structure, with field-major orientation of nodes; all values are stored in one contiguous array on device
@@ -191,6 +211,9 @@ class StaticPSTCPUIter : public StaticPrioritySearchTree<T, PointStructTemplate,
 			RIGHT_SEARCH,
 			THREE_SEARCH
 		};
+
+
+	// Friend function declarations
 
 	/*
 		For friend functions of template classes, for the compiler to recognise the function as a template function, it is necessary to either pre-declare each template friend function before the template class and modify the class-internal function declaration with an additional <> between the operator and the parameter list; or to simply define the friend function when it is declared
